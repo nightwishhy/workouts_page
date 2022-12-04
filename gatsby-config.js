bb321c40b9bba7d418c2b5815692f850e24852ae
nightwishhy <nightwishhy@126.com>
2022-12-04 09:24:17 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: '/workouts_page', // Change to `/workouts_page` when running on GitHub pages
   siteMetadata: {
     siteTitle: 'Yao\'s workout records',
-    siteUrl: 'https://ben29.xyz',
+    siteUrl: 'https://nightwishhy.github.io/workouts_page/',
     logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
     description: 'Personal Workouts Page',
     keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
